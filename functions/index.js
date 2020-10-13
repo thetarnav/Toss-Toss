f659bd06626c8db52281a8da87ec45baf7d2e97d
thetarnav <gthetarnav@gmail.com>
2020-10-13 16:41:28 +0200

    crontab cleanup cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'),
+	admin = require('firebase-admin')
 
+admin.initializeApp()
+
+const firestore = admin.firestore()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +11,15 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.clearUpOldSessions = functions.pubsub
+	.schedule('0 */2 * * *')
+	.timeZone('Europe/Warsaw')
+	.onRun(context => {
+		firestore
+			.collection('sessions')
+			.where('timestamp', '<', Date.now() - 3600000)
+			.get()
+			.then(snapshot => snapshot.forEach(doc => doc.ref.delete()))
+		return null
+	})
